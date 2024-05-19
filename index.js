6adb08669c55257686d853e3837adc1766a89d0f
Juan Sebastian Perniola <jperniola@nigul.cide.es>
2024-05-19 12:29:41 +0200

    add insert comandes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const db = require('better-sqlite3')('personas.sqlite');
+const db = require('better-sqlite3')('personas.sqlite'); // Add this line to initialize the db object
 const app = express();
 const port = 3000;
 app.use(express.json());
@@ -19,6 +19,7 @@ app.get('/persona', (req, res) => {
 app.post('/persona', (req, res) => {
   const error = "error";
   try {
+    console.log(req.body);
     const statement = db.prepare('INSERT INTO Usuario(id, nombre, email) VALUES(?, ?, ?)');
     const info = statement.run(req.body.id, req.body.nombre, req.body.email);
     res.redirect("persona");
@@ -39,31 +40,13 @@ app.get('/crearProducto', (req, res) => {
 
 app.post('/crearProducto', (req, res) => {
   try {
-    const statement = db.prepare('INSERT INTO product(id, nombre, preu) VALUES(?, ?, ?)');
+    console.log(req.body);
+    const statement = db.prepare('INSERT INTO product(id, Nombre, Preu) VALUES(?, ?, ?)');
     const info = statement.run(req.body.id, req.body.nombre, req.body.preu);
-    res.redirect('/product');
-  } catch (SqliteError) {
-
-  }
-});
-
-
-// Rutas para Comandas
-app.get('/comandes', (req, res) => {
-  const rowsProduct = db.prepare('SELECT * FROM Product').all();
-  const rowsPersona = db.prepare('SELECT * FROM Usuario').all();
-  res.render('comandes', { personas: rowsPersona, productos: rowsProduct });
-});
-
-app.post('/comandes', (req, res) => {
-  try {
-  const statement = db.prepare('INSERT INTO Comandes(Usuario_ID, product_ID) VALUES(?, ?)');
-  const info = statement.run(req.body.Usuario_ID, req.body.product_ID);
-  console.log(req.body);
-  console.log(info);
-  res.redirect('/comandes');
+    res.redirect('/crearProducto');
   } catch (SqliteError) {
     console.log(SqliteError);
+    res.status(500).send("Error inserting data into Product table");
   }
 });
 
@@ -72,6 +55,30 @@ app.get('/showComandas', (req, res) => {
   const rows = db.prepare('SELECT Usuario.nombre as UsuarioNombre, Usuario.email, product.nombre as ProductName, product.preu FROM Comandes JOIN Usuario ON Comandes.Usuario_ID = usuario.id JOIN product on product.id = Comandes.product_ID').all();
   res.render('showComandas', { comandes: rows });
 });
+
+// Rutas para Comandas
+app.get('/comandes', (req, res) => {
+  const rowsProduct = db.prepare('SELECT * FROM Product').all();
+  const rowsPersona = db.prepare('SELECT * FROM Usuario').all();
+  res.render('comandes', { personas: rowsPersona, productos: rowsProduct });
+});
+
+
+app.post('/comandes', (req, res) => {
+  try {
+    console.log(req.body);
+    const statement = db.prepare('INSERT INTO Comandes(Usuario_ID, Product_ID) VALUES(?, ?)');
+    const info = statement.run(req.body.Usuario_ID, req.body.product_ID);
+    console.log(info);
+    res.redirect('/showComandas');
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error inserting data into Comandes table");
+  }
+});
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}/persona`);
